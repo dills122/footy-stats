1a5e65bd43aab1ba4386572c494504e052f0ad8e
Dylan Steele <dylansteele57@gmail.com>
2025-09-20 16:45:59 -0400

    pulled in eslint/prettier changes from template

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,21 @@
-import type { Config } from "jest";
+import type { Config } from 'jest';
 
 // If you want to use paths from tsconfig, enable resolveJsonModule in tsconfig, or just hardcode for now
 
 const config: Config = {
-	preset: "jest-preset-angular",
-	transform: {
-		"^.+\\.(ts|js|mjs|html|svg)$": [
-			"jest-preset-angular",
-			{
-				tsconfig: "<rootDir>/tsconfig-esm.spec.json",
-				stringifyContentPathRegex: "\\.(html|svg)$",
-				useESM: true,
-			},
-		],
-	},
-	setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-	testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
+  preset: 'jest-preset-angular',
+  transform: {
+    '^.+\\.(ts|js|mjs|html|svg)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig-esm.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+        useESM: true,
+      },
+    ],
+  },
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
 };
 
 export default config;
