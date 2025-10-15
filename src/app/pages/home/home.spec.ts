4e9b80ad99470a87ba9652c3e3342619ad59f1bd
Dylan Steele <dylansteele57@gmail.com>
2025-10-14 21:13:29 -0400

    Added Home/landing page, updated routing

--- src/app/pages/home/home.spec.ts
+++ src/app/pages/home/home.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { Home } from './home';
 
 describe('Home', () => {
@@ -9,6 +10,7 @@ describe('Home', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Home],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Home);
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,14 @@
+// home.component.ts
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrls: ['./home.scss'],
+  imports: [CommonModule, MatCardModule, MatButtonModule, RouterLink],
 })
 export class Home {}
