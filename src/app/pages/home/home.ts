dabe6fd5ae69c31f3fbf299901440197c09ce4f4
Dylan Steele <dylansteele57@gmail.com>
2025-10-28 19:29:26 -0400

    start homepage refactor

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,14 +1,15 @@
 // home.component.ts
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
-  imports: [CommonModule, MatCardModule, MatButtonModule, RouterLink],
+  imports: [CommonModule, MatCardModule, MatButtonModule, RouterLink, MatIconModule],
 })
 export class Home {}
